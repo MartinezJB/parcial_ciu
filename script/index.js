896bdcb2ea73a6e2f957e34d377de910cc4900e8
MartinezJB <martinez.joaquin.b@gmail.com>
2021-09-27 01:30:34 -0300

    first commit

---
+++ script/index.js
@@ -0,0 +1,35 @@
+let botonIngreso = document.querySelector("#entrar");
+
+const tirarMensaje = function(lugarDondeMostrar, mensaje) {
+    if(document.querySelector(lugarDondeMostrar).textContent != mensaje){
+        document.querySelector(lugarDondeMostrar).innerHTML = `<p class="mensaje-error">${mensaje}</p>`;
+    }
+    
+}
+
+const validarDatos = function() {
+    event.preventDefault();
+    let user = document.querySelector("#user").value;
+    let password = document.querySelector("#password").value;
+    
+    if( esUsuarioValido(user) && esContraseñaValida(password) ){
+        window.location.href = "./principal.html"
+    }else {
+        mostrarErrores(user, password)
+    }
+}
+
+const esUsuarioValido = (user) => !isNaN(user);
+
+const esContraseñaValida = (contra) => contra.length >= 4;
+
+function mostrarErrores(user, password) {
+    isNaN(user) ? tirarMensaje("#erroresUsuario", "El usuario solo puede ser números.")
+        : tirarMensaje("#erroresUsuario", "");
+
+    password.length < 4 ? tirarMensaje("#erroresContraseña", "Contraseña demasiado corta.")
+        : tirarMensaje("#erroresContraseña", "");
+}
+
+
+botonIngreso.addEventListener("click", validarDatos);
---
+++ script/principal.js
@@ -0,0 +1,3 @@
+document.querySelector("#salir").addEventListener("click", function() {
+    window.location.href = "./index.html"
+})
